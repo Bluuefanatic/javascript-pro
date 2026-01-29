df3f916290fc177be650533715ace915e2525922
Bluuefanatic <izirenjoel@gmail.com>
2026-01-29 15:56:49 +0100

    add second triangle object with area and hypotenuse methods

--- oop/triangle.js
+++ oop/triangle.js
@@ -17,3 +17,14 @@ let myTri = {
         return Math.sqrt(this.a ** 2 + this.b ** 2);
     },
 };
+
+let myTri2 = {
+    a: 5,
+    b: 12,
+    getArea: function () {
+        return (this.a + this.b) / 2;
+    },
+    getHypotenuse: function () {
+        return Math.sqrt(this.a ** 2 + this.b ** 2);
+    },
+};
