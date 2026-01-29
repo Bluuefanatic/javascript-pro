9be503896993b384fb874c21d5eb7fa9f1df424e
Bluuefanatic <izirenjoel@gmail.com>
2026-01-29 15:55:32 +0100

    add methods to calculate area and hypotenuse in triangle object

--- oop/triangle.js
+++ oop/triangle.js
@@ -5,3 +5,15 @@ function getTriangleArea(a, b) {
 function getTriangleHypotenuse(a, b) {
     return Math.sqrt(a ** 2 + b ** 2);
 }
+
+
+let myTri = {
+    a: 3,
+    b: 4,
+    getArea: function () {
+        return (this.a + this.b) / 2;
+    },
+    getHypotenuse: function () {
+        return Math.sqrt(this.a ** 2 + this.b ** 2);
+    },
+};
