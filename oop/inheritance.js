7f9f5ee101fbf79f73af67043c71aeb923bc9ebf
Bluuefanatic <izirenjoel@gmail.com>
2026-01-31 02:56:37 +0100

    feat: implement Triangle class with area and hypotenuse methods

---
+++ oop/inheritance.js
@@ -0,0 +1,17 @@
+class Triangle {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    getArea() {
+        return (this.a + this.b) / 2;
+    }
+    getHypotenuse() {
+        return Math.sqrt(this.a ** 2 + this.b ** 2);
+    }
+
+    describe() {
+        return `i am a triangle with area of ${this.getArea()}`;
+    }
+}
