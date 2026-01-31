f56f9807810cec95aa98e30664ebdbc53448e6ec
Bluuefanatic <izirenjoel@gmail.com>
2026-01-31 03:05:19 +0100

    feat: add ShyTriangle class with area, hypotenuse, and describe methods

--- oop/inheritance.js
+++ oop/inheritance.js
@@ -15,3 +15,22 @@ class Triangle {
         return `i am a triangle with area of ${this.getArea()}`;
     }
 }
+
+
+class ShyTriangle {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    getArea() {
+        return (this.a + this.b) / 2;
+    }
+    getHypotenuse() {
+        return Math.sqrt(this.a ** 2 + this.b ** 2);
+    }
+
+    describe() {
+        return `(runs and hides)`;
+    }
+}
