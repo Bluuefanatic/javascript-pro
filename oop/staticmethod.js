ce66abcab60a94e0c975a85a02545a3fa5171444
Bluuefanatic <izirenjoel@gmail.com>
2026-02-03 18:14:31 +0100

    feat: add static meow method to cat class

--- oop/staticmethod.js
+++ oop/staticmethod.js
@@ -3,6 +3,9 @@ class cat {
         this.name = name;
         this.breed = breed;
     }
+    static meow() {
+        return "meow!";
+    }
 
     static species = "felis catus"; // static property
 }
