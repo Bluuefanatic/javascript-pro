13e7203e413576f9200ddc0fc70f455c82c3a6d6
Bluuefanatic <izirenjoel@gmail.com>
2026-02-03 19:35:43 +0100

    fix: correct logging in meow method to display context properly

--- oop/staticmethod.js
+++ oop/staticmethod.js
@@ -4,7 +4,7 @@ class cat {
         this.breed = breed;
     }
     static meow() {
-        console.log("THIS IS; this");
+        console.log("THIS IS", this);
     }
     static species = "felis catus"; // static property
 }
