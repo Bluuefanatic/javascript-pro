466e2f47a66c09e62d2e92c9058bc552d21b1834
Bluuefanatic <izirenjoel@gmail.com>
2026-02-05 22:26:35 +0100

    fix: add getter for color property in circle class

--- oop2/setters.js
+++ oop2/setters.js
@@ -7,6 +7,7 @@ class circle {
     get radius() {
         return this._radius;
     }
+
     //setter for the radius
     set radius(value) {
         if (value < 0) {
@@ -14,5 +15,9 @@ class circle {
         } else {
             this._radius = value;
         }
+    }
+
+    get color() {
+        return this._color;
     }
 }
