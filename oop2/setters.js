1e7337fa1fc0d363af1ad2dec3d35b56a63c1560
Bluuefanatic <izirenjoel@gmail.com>
2026-02-06 15:38:44 +0100

    fix: remove unnecessary blank lines in circle class constructor

--- oop2/setters.js
+++ oop2/setters.js
@@ -5,10 +5,7 @@ class circle {
         this._radius = radius;
         this.setColor(color);
     }
-
-
-
-
+    
     setColor(newColor) {
         if (circle.validColors.has(newColor)) {
             this._color = newColor;
