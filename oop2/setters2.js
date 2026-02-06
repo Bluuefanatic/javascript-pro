283cc262cceaac3ad916ae6358900b0893213ece
Bluuefanatic <izirenjoel@gmail.com>
2026-02-06 17:47:10 +0100

    fix: format User class and improve getter method

--- oop2/setters2.js
+++ oop2/setters2.js
@@ -1,10 +1,10 @@
-class User{
-    constructor (firstName, lastName){
+class User {
+    constructor(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-get fullName(){
-    return `${this.firstName} ${this.lastName} `
-}
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
 }
