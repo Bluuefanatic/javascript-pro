25985632c694494778e45eb291ab276ea5893980
Bluuefanatic <izirenjoel@gmail.com>
2026-02-09 22:32:09 +0100

    feat: rename class to MyClass and implement public method to access private method

--- oop2/privatemethod.js
+++ oop2/privatemethod.js
@@ -1,4 +1,11 @@
-class myClass {
-    #privateMethod() {        
+class MyClass {
+    #privateMethod() {
+    }
+
+    publicMethod() {
+        return this.#privateMethod();
     }
 }
+
+const myClass = new MyClass();
+console.log(myClass.publicMethod());
