81c8c61e9d8752164f4cd1c796ea41e12f4929e8
Bluuefanatic <izirenjoel@gmail.com>
2026-02-13 15:28:25 +0100

    fix: comment out unused person object

--- this/this.js
+++ this/this.js
@@ -1,10 +1,10 @@
-const person = {
+/*const person = {
     name: 'Alice',
     city: 'los angeles',
     sing: function () {
         return `${this.name} in ${this.city} sings la la la`;
     }
-}
+}*/
 
 class Person {
     constructor(firstName) {
