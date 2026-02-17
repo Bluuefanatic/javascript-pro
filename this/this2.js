e2c2bf2488317476d90c2965735d32ae97197ec9
Bluuefanatic <izirenjoel@gmail.com>
2026-02-17 13:37:21 +0100

    fix: add exclamation mark to greeting output in joe object

--- this/this2.js
+++ this/this2.js
@@ -1,6 +1,6 @@
 const joe = {
     firstName: 'joe',
     greet: function (greeting) {
-        console.log(`${this.firstName} says ${greeting}`);
+        console.log(`${this.firstName} says ${greeting}!`);
     }
 }
