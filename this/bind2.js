83706975c337bb1f928b3c16cdafaddc7d80b803
Bluuefanatic <izirenjoel@gmail.com>
2026-02-19 23:11:52 +0100

    feat: add conan object with sing method and update button event listener in bind2.js

--- this/bind2.js
+++ this/bind2.js
@@ -4,3 +4,15 @@ btn.addEventListener("click", function () {
     console.log("Button clicked!");
 })
 */
+
+const conan = {
+    name: 'conan',
+    city: 'los angeles',
+    sing: function () {
+        console.log("this is: ", this);
+        return `${this.name} in ${this.city} sings la la la`;
+    }
+}
+
+const btn = document.querySelector("#clickMe");
+btn.addEventListener("click", conan.sing);
