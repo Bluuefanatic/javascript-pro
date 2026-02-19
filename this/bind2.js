e3d842bf5a1e5a22d66ceee173547d6fc3400d6c
Bluuefanatic <izirenjoel@gmail.com>
2026-02-19 22:42:41 +0100

    fix: assign button element to variable and update event listener in bind2.js

--- this/bind2.js
+++ this/bind2.js
@@ -1,4 +1,4 @@
-document.querySelector("#clickMe");
-button.addEventListener("click", function () {
+const btn = document.querySelector("#clickMe");
+btn.addEventListener("click", function () {
     console.log("Button clicked");
 })
