e69312d494317b62f0cebd3159324c03bf0bfa1d
Bluuefanatic <izirenjoel@gmail.com>
2026-02-24 00:47:07 +0100

    fix: remove unnecessary blank lines and comment out Dog function in new.js

--- oop3/new.js
+++ oop3/new.js
@@ -13,21 +13,13 @@ class dog {
 
 
 
-
-
-
-
-
-
-
-
-
-/*function Dog(name, breed) {
+function Dog(name, breed) {
     console.log("THIS IS: ", this);
     this.name = name;
     this.breed = breed;
 }
 
+/*
 function user(username, email) {
     this.username = username;
     this.email = email;
