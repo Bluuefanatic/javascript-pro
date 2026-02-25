9b3305acaa8a5b44ea1b6778fa4da2e7dc278e48
Bluuefanatic <izirenjoel@gmail.com>
2026-02-25 06:17:59 +0100

    refactor: uncomment and clean up Dog function and related code in new.js

--- oop3/new.js
+++ oop3/new.js
@@ -1,3 +1,4 @@
+/*
 const myObj = {
     color: "purple",
     score: 99,
@@ -5,6 +6,15 @@ const myObj = {
         console.log("HI!!!");
     }
 }
+*/
+
+/*
+function user(username, email) {
+    this.username = username;
+    this.email = email;
+    this.isAdmin = false;
+}
+*/
 
 /*
 class dog {
@@ -21,23 +31,17 @@ class dog {
 }
 */
 
-/*
+
 function Dog(name, breed) {
     this.name = name;
     this.breed = breed;
-    this.bark = function () {
-        return `${this.name} says woof!`;
-    }
-    this.sleep = function () {
-        return `${this.name} is sleeping!`;
-    }
-}
-*/
 
-/*
-function user(username, email) {
-    this.username = username;
-    this.email = email;
-    this.isAdmin = false;
 }
-*/
+
+Dog.prototype.bark = function () {
+    return `${this.name} says woof!`;
+}
+
+Dog.prototype.sleep = function () {
+    return `${this.name} is sleeping!`;
+}
