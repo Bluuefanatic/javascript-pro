742a16fe64c8a85b2031fd22410561f46954a0e3
Bluuefanatic <izirenjoel@gmail.com>
2026-03-02 23:36:24 +0100

    style: update comments to clarify promise usage in promises.js

--- asynchronous/promises.js
+++ asynchronous/promises.js
@@ -1,13 +1,14 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
 const url = `${BASE_URL}/1`;
-// using promises
-fetch(url)
-    .then(function (data) {
-        console.log(data);
-    })
-    .catch(function (err) {
-        console.log("ERR!!", err);
-    })
+
+//****using promises****\\
+//fetch(url)
+//  .then(function (data) {
+//    console.log(data);
+//})
+//.catch(function (err) {
+//  console.log("ERR!!", err);
+//})
 
 // same as above but using arrow functions
 fetch(url)
