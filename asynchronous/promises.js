46a98e826c058570e5c90424981ce9b46f1d6c07
Bluuefanatic <izirenjoel@gmail.com>
2026-03-02 23:17:05 +0100

    style: format fetch call for better readability in promises.js

--- asynchronous/promises.js
+++ asynchronous/promises.js
@@ -1,8 +1,10 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
 const url = `${BASE_URL}/1`;
 
-fetch(url).then(function (data) {
-    console.log(data);
-}).catch(function (err) {
-    console.log("ERR!!", err);
-})
+fetch(url)
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (err) {
+        console.log("ERR!!", err);
+    })
