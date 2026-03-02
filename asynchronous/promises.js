89cd41cecc78222b8d266bbf643582d56653d95a
Bluuefanatic <izirenjoel@gmail.com>
2026-03-02 23:33:46 +0100

    style: add comments for clarity in promises.js

--- asynchronous/promises.js
+++ asynchronous/promises.js
@@ -1,6 +1,6 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
 const url = `${BASE_URL}/1`;
-
+// using promises
 fetch(url)
     .then(function (data) {
         console.log(data);
@@ -9,6 +9,7 @@ fetch(url)
         console.log("ERR!!", err);
     })
 
+// same as above but using arrow functions
 fetch(url)
     .then((res) => console.log(res))
     .catch((err) => console.log("ERR!!", err));
