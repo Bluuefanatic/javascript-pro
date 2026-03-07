4d34e2ece9a227aed2e030288b6893d1ab0da5be
Bluuefanatic <izirenjoel@gmail.com>
2026-03-07 22:29:48 +0100

    add getFakeWeb function with error handling for fetching data

--- asynchronous/asyncawait.js
+++ asynchronous/asyncawait.js
@@ -2,3 +2,12 @@ async function hello() {
     const result = await fetch(`${BASE_URL}/1`)
     console.log(result)
 }
+
+async function getFakeWeb() {
+    try {
+        const res = await fetch('https://fakeweb.com')
+        console.log(res)
+    } catch (e) {
+        console.log('SOMETHING WENT WRONG!!')
+    }
+}
